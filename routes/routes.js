e7709b28029d39e96f4dc145d8ee58a14f380364
Thib-numberX <peronnothibault@hotmail.fr>
2023-04-01 11:41:19 +0200

    add try catch

--- routes/routes.js
+++ routes/routes.js
@@ -12,15 +12,34 @@ const params = {
 // attention ne pas inverser req et res, sinon erreur
 router.get("/", async (req, res) =>{
     /*res.json({test: 'la route get'})*/
-   
-        const response = await fetch(
-          'https://trefle.io/api/auth/claim', {
-            method: 'post',
-            body: JSON.stringify(params),
-            headers: { 'Content-Type': 'application/json' }
-          });
-        const json = await response.json();
-        res.json(json);
+    try{
+      const response = await fetch(
+        'https://trefle.io/api/auth/claim', {
+          method: 'post',
+          body: JSON.stringify(params),
+          headers: { 'Content-Type': 'application/json' }
+        });
+      const json = await response.json();
+      res.json(json);
+
+    }catch (error) {
+      if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+      } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+      } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+      }
+      console.log(error.config);
+  }
 });
 
 module.exports = router;
