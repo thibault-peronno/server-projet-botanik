9d9d67e56211886445733122e84416e4cc3b58d8
Thib-numberX <peronnothibault@hotmail.fr>
2023-04-10 16:35:03 +0200

    add or 5000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const port = process.env.port;
+const port = process.env.port || 5000;
 
 const app = express();
 
